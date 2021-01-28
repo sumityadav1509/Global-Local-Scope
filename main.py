f3e1613dd255051a2fc841774b3a43350ad470ee
sumityadav1509 <sumityadav2005@gmail.com>
2021-01-28 18:53:19 +0000

    Understanding global and local scope.

--- main.py
+++ main.py
@@ -1,61 +1,61 @@
-# ################### Scope ####################
+################### Scope ####################
 
-# enemies = 1
+enemies = 1
 
-# def increase_enemies():
-#     enemies = 2
-#     print(f"enemies inside function: {enemies}")
+def increase_enemies():
+    enemies = 2
+    print(f"enemies inside function: {enemies}")
 
-# increase_enemies()
-# print(f"enemies outside function: {enemies}")
+increase_enemies()
+print(f"enemies outside function: {enemies}")
 
-# # Local Scope
+# Local Scope
 
-# def drink_potion():
-#     potion_strength = 2
-#     print(potion_strength)
+def drink_potion():
+    potion_strength = 2
+    print(potion_strength)
 
-# drink_potion()
-# print(potion_strength)
+drink_potion()
+print(potion_strength)
 
-# # Global Scope
-# player_health = 10
+# Global Scope
+player_health = 10
 
-# def game():
-#     def drink_potion():
-#         potion_strength = 2
-#         print(player_health)
+def game():
+    def drink_potion():
+        potion_strength = 2
+        print(player_health)
 
-#     drink_potion()
+    drink_potion()
 
-# print(player_health)
+print(player_health)
 
-# # There is no Block Scope
+# There is no Block Scope
 
-# game_level = 3
+game_level = 3
 
-# def create_enemy():
-#     enemies = ["Skeleton", "Zombie", "Alien"]
-#     if game_level < 5:
-#         new_enemy = enemies[0]
+def create_enemy():
+    enemies = ["Skeleton", "Zombie", "Alien"]
+    if game_level < 5:
+        new_enemy = enemies[0]
 
-#     print(new_enemy)
+    print(new_enemy)
 
 
-# # Modifying Global Scope
+# Modifying Global Scope 
 
-# enemies = 1
+enemies = 1
 
-# def increase_enemies():
-#     print(f"enemies inside function: {enemies}")
-#     return enemies + 1
+def increase_enemies():
+    print(f"enemies inside function: {enemies}")
+    return enemies + 1
 
-# enemies = increase_enemies()
-# print(f"enemies outside function: {enemies}")
+enemies = increase_enemies()
+print(f"enemies outside function: {enemies}")
 
-# #Global Constants
+#Global Constants
 
-# PI = 3.14159
-# URL = "https://www.google.com"
-# TWITTER_HANDLE = "@yu_angela"
+PI = 3.14159
+URL = "https://www.google.com"
+TWITTER_HANDLE = "@yu_angela"
 
